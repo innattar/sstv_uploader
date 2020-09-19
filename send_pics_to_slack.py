77063493dfdd6f10ee9610e9c5ff2e504dccf07b
Rich Rattanni <rattanni@gmail.com>
2020-09-19 15:11:54 -0400

    Put metadata in image if available from QSSTV

--- send_pics_to_slack.py
+++ send_pics_to_slack.py
@@ -41,6 +41,15 @@ def new_pics():
     log.debug("After filtering, candidate upload list is: {}".format(candidates))
     return candidates
 
+def append_metadata(pic_list):
+    for p in pic_list:
+        f = Path(os.path.join(SSTV_RX_DIR, p +".meta"))
+        if f.exists():
+            metadata = f.read_text()
+            image_file = os.path.join(SSTV_RX_DIR, p)
+            im = "convert {} -background YellowGreen label:'{}' -gravity Center -append {}".format(image_file, str(metadata), image_file)
+            subprocess.run(shlex.split(im), capture_output=False)
+
 def upload(files, to_thread=False):
     if to_thread:
         curl_cli = 'curl -X POST --data \'{{"channel":"{}","text":"New batch of images"}}\' -H \'Content-type: application/json\'  -H "Authorization: Bearer {}" https://slack.com/api/chat.postMessage'.format(CHANNEL, TOKEN)
@@ -71,6 +80,7 @@ def poll(interval=60, upload_threshold=1, timeout_upload_period_s = 3600.0):
     while True:
         time.sleep(interval)
         pics_to_upload = new_pics()
+        append_metadata(pics_to_upload)
         if len(pics_to_upload) == 0:
             log.debug("No images have been received")
         elif len(pics_to_upload) >= upload_threshold or ((time.monotonic() - last_upload) > timeout_upload_period_s):
